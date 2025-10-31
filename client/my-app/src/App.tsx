5c898d4569d11dcc5e0aa13e309dfde8da47fa53
Nguyen Tri <ntri86679@gmail.com>
2025-10-31 20:04:00 +0700

    create project

---
+++ client/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-900">
+      <h1 className="text-4xl font-bold text-white animate__animated animate__bounce">
+        🚀 Hello Tailwind Animate!
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ client/my-app/tailwind.config.js
@@ -0,0 +1,13 @@
+import twAnimate from "tw-animate-css"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [twAnimate],
+}
